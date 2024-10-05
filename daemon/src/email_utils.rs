155944495b0994b9502987524f3e8769915bc16a
cromalu <cromalu3@gmail.com>
2024-10-05 18:48:22 +0200

    fix: set email send timeout

--- daemon/src/email_utils.rs
+++ daemon/src/email_utils.rs
@@ -1,10 +1,13 @@
 use std::net::IpAddr;
 use std::process::Stdio;
-use log::debug;
+use std::time::Duration;
+use log::{debug, error};
 use serde::{Deserialize, Serialize};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::process::Command;
+use tokio::time::timeout;
 use crate::common;
+use crate::common::Error;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct EmailConfig {
@@ -12,6 +15,7 @@ pub struct EmailConfig {
     pub sender_alias: String,
     pub server_domain: String,
     pub internet_host: IpAddr,
+    pub email_send_timeout_sec: u64,
 }
 
 #[derive(Debug)]
@@ -24,33 +28,39 @@ pub struct OutgoingEmail {
 ///Email sending is done through sendmail pre-installed binary on host,
 /// configured from /etc/ssmtp/ssmpt.conf and /etc/ssmtp/revaliases configuration files
 pub async fn send_email(config: &EmailConfig, email: &OutgoingEmail) -> common::Result<()> {
-    let content = format!("Subject: {}\n\n{}", email.title, email.msg);
+    timeout(
+        Duration::from_secs(config.email_send_timeout_sec),
+        async {
+            let content = format!("Subject: {}\n\n{}", email.title, email.msg);
+            let mut send_mail_process = Command::new(&config.binary_file)
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .arg("-v")
+                .arg(&email.to)
+                .arg("-F")
+                .arg(&config.sender_alias)
+                .spawn()?;
+            let mut stdin = send_mail_process.stdin.take().ok_or(common::Error::SystemCommandExecutionError)?;
+            let mut stderr = send_mail_process.stderr.take().ok_or(common::Error::SystemCommandExecutionError)?;
+            stdin
+                .write(content.as_bytes())
+                .await?;
+            // We drop the handle here which signals EOF to the child process.
+            // This tells the child process that it there is no more data on the pipe.
+            drop(stdin);
 
+            let output = send_mail_process.wait_with_output().await?;
 
-    let mut send_mail_process = Command::new(&config.binary_file)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .arg("-v")
-        .arg(&email.to)
-        .arg("-F")
-        .arg(&config.sender_alias)
-        .spawn()?;
-    let mut stdin = send_mail_process.stdin.take().ok_or(common::Error::SystemCommandExecutionError)?;
-    let mut stderr = send_mail_process.stderr.take().ok_or(common::Error::SystemCommandExecutionError)?;
-    stdin
-        .write(content.as_bytes())
-        .await?;
-    // We drop the handle here which signals EOF to the child process.
-    // This tells the child process that it there is no more data on the pipe.
-    drop(stdin);
+            //send mail output is printed to stderr
+            let mut s = String::new();
+            let _ = stderr.read_to_string(&mut s).await?;
+            debug!("send_email: command output :\n{}", s);
 
-    let output = send_mail_process.wait_with_output().await?;
-
-    //send mail output is printed to stderr
-    let mut s = String::new();
-    let _ = stderr.read_to_string(&mut s).await?;
-    debug!("send_email: command output :\n{}", s);
-
-    output.status.success().then(|| ()).ok_or(common::Error::SystemCommandExecutionError)
+            output.status.success().then(|| ()).ok_or(common::Error::SystemCommandExecutionError)
+        })
+        .await.unwrap_or_else(|_| {
+            error!("send_email: timeout while trying to send email");
+            Err(Error::SystemCommandExecutionError)
+        })
 }
